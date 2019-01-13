9d702b11b9c16210e8e7fc6e435e1a06b1db8005
mnshashank <shashank.1si12cs111@gmail.com>
2019-01-13 19:48:33 +0530

    Question resource added and also add question modal now adds new data dynamically

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  resources :questions
+
   root 'home#index'
+
   get '/about' => 'home#about'
-  post '/questions' => 'home#temp'
-
-  get '/questions/:id' => 'home#question'
-
-  post 'answers' => 'home#temp'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
