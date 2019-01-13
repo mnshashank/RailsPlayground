c4f2b68fd74df3e1d4962fdce8966b7e21867bb1
mnshashank <shashank.1si12cs111@gmail.com>
2019-01-13 16:22:21 +0530

    basic bootstrap view with minimal functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get '/about' => 'home#about'
+  post '/questions' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
+
+  post 'answers' => 'home#temp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
