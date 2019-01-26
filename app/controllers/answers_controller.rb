923c3101bc5d0a05a70650a0a10f7cdddebbb1ba
mnshashank <shashank.1si12cs111@gmail.com>
2019-01-26 15:11:34 +0530

    more changes to the UI and mailing, active job functionalities added

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,10 @@ class AnswersController < ApplicationController
 
     def create 
         question_var = Question.find(params[:answer][:question_id])
-        question_var.answers.create(answer_params)
+        answer = question_var.answers.create(answer_params)
+
+        MainMailer.notify_question_author(answer).deliver_later
+        session[:current_user_email] = answer_params[:email]
         redirect_to question_var
     end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def current_user_email
+        session[:current_user_email] || 'No email is saved !'
+    end
 end
